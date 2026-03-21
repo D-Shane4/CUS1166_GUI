2cc124494473a621cfda94caf3a1932f03845c86
Shanti. W <Shantiwright31@gmail.com>
2026-03-20 21:50:33 -0400

    job
    Change-Id: I7485cbf9bab0a61931dbe7e21e57f804436f3745

--- Job.java
+++ Job.java
@@ -2,34 +2,64 @@ import java.time.LocalDateTime;
 
 public class Job {
 
-    private String jobID;
-    private String clientID;
-    private int duration;
-    private LocalDateTime deadline;
-    private String status;
-
-    public Job(String jobID, String clientID, int duration, LocalDateTime deadline) {
+    private String jobID;              
+    private String clientID;             
+    private LocalDateTime arrivalTime; 
+    private LocalDateTime departureTime;
+    private LocalDateTime deadline; 
+    private int durationMinutes;     
+   
+    
+    // Constructor
+    public Job(String jobID, String clientID, LocalDateTime arrivalTime, LocalDateTime departureTime, int durationMinutes, LocalDateTime deadline ) {
         this.jobID = jobID;
         this.clientID = clientID;
-        this.duration = duration;
+        this.arrivalTime = arrivalTime;
+        this.departureTime = departureTime;
+        this.durationMinutes = durationMinutes;
         this.deadline = deadline;
-       
+        
+
     }
 
-    public String getJobID() { return jobID; }
-    public String getClientID() { return clientID; }
-    public int getDuration() { return duration; }
-    public LocalDateTime getDeadline() { return deadline; }
-    public String getStatus() { return status; }
+    // returns job ID
+    public String getJobID() {
+        return jobID;
+    }
+
+    // returns client object
+    public String getClientID() {
+        return clientID;
+    
+    }
+
+    // returns when job started
+    public LocalDateTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public LocalDateTime getDepartureTime() {
+    	return departureTime;
+    }
+    
+    public LocalDateTime getDeadline() {
+    	return deadline;
+    }
+    
+    // returns job duration in minutes
+    public int getDurationMinutes() {
+        return durationMinutes;
+    }
 
     
-    
-
+    // formats job data for saving to file
     public String fileText() {
         return "Job ID: " + jobID +
                " | Client ID: " + clientID +
-               " | Duration: " + duration + " min" +
+               " | Arrival Time: " + arrivalTime +
+               " | Departure Time: " + departureTime+
                " | Deadline: " + deadline +
-               " | Status: " + status;
+               " | Duration (min): " + durationMinutes;
+   
     }
 }
